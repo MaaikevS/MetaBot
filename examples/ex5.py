6c285ced03802de7290f0615908a7a1b64e55fde
MaaikevS <31585951+MaaikevS@users.noreply.github.com>
2022-04-27 20:57:25 +0200

    Update ex5.py

--- examples/ex5.py
+++ examples/ex5.py
@@ -12,13 +12,14 @@ Example script to upload instances to the Knowledge Graph Editor
 
 import glob
 import os
+from getpass import getpass
 from metabot import openMINDS_wrapper
 
-w = openMINDS_wrapper
+w = openMINDS_wrapper()
 
 # Specify the folder where the instances are stored
 file_location = os.getcwd()
-token = input("Please enter your KG token: ")
+token = getpass(prompt="Please enter your KG token (or Enter to skip uploading to the KG): ")
 instances_fnames = glob.glob(file_location + "*\\*", recursive = True)
 
 # Select the space the instances need to be uploaded to, e.g. dataset
